1fd03f64884729b29c51671dc2219d6d77101c59
Muhammad Khan <mk_muhammad.khan@hotmail.com>
2012-09-03 13:32:25 -0500

    final commit of the day!
    changed and updated the file to work with everything. updated readme
    with a small tutorial on how to use. hopefully push it to my blog. This
    weekend!. Thanks

--- mk_multiedit.js
+++ mk_multiedit.js
@@ -1,33 +1,28 @@
 var mk_multiedit = {
-	
 	config : {
-		mainID : '.test',
-		parent : '#todoItem'
+		mainID : '.test', // attach the submit and cancel button to
+		parent : '#todoItem' // what elements are edit
 	},
-
 	json : '',
 
 	init : function (config) {
 		$.extend(mk_multiedit.config, config)
-		$(mk_multiedit.config.mainID).on('click', mk_multiedit.make_editable);	
+		$(mk_multiedit.config.parent).on('click', mk_multiedit.make_editable);	
 		mk_multiedit.submit();
 	},
-
 	make_editable : function () {
-				$(mk_multiedit.config.mainID).each(function () {
-					var x = $(this).text();
-					var test = $('.this');
+				$(mk_multiedit.config.parent).each(function () {
+					var this_text = $(this).text();
 					$(this).text("");
 					var data_json = $(this).parent().find('input').data('json')
-					$('<input />', { type: 'text', class: 'edit', value: x, 'data-json': data_json}).appendTo(this);
+					$('<input />', { type: 'text', class: 'edit', value: this_text, 'data-json': data_json}).appendTo(this);
 				});
-				$('<input/>', {type: 'submit', class: 'submit', value: 'submit!'}).appendTo('#todoItem');
-				$('<input/>', {type: 'submit', class: 'cancel', value: 'cancel!'}).appendTo('#todoItem');
-				$(mk_multiedit.config.mainID).off('click');
+				$('<input/>', {type: 'submit', class: 'submit', value: 'submit!'}).appendTo(mk_multiedit.config.mainID);
+				$('<input/>', {type: 'submit', class: 'cancel', value: 'cancel!'}).appendTo(mk_multiedit.config.mainID);
+				$(mk_multiedit.config.parent).off('click');
 	}, 
-
 	submit : function () {
-		$(mk_multiedit.config.parent).on('click', '.submit', function () {
+		$(mk_multiedit.config.mainID).on('click', '.submit', function () {
 			var key = new Array();
 			var value = new Array();
 			$('.edit').each(function ( index ) {
@@ -35,20 +30,17 @@ var mk_multiedit = {
 				key[$(this).data('json')] = $(this).val();
 			});
 			this.json = mk_multiedit.form_to_json(key, value, false);
-			console.log(this.json);
 			mk_multiedit.complete();
 		});
 	}, 
-
 	complete : function () {
-		$(mk_multiedit.config.mainID).each(function () {
+		$(mk_multiedit.config.parent).each(function () {
 			$(this).text( $(this).find('.edit').val() );
 		});
 		$('.cancel').remove();
 		$('.submit').remove();
-		$(mk_multiedit.config.mainID).on('click', mk_multiedit.make_editable);	
+		$(mk_multiedit.config.parent).on('click', mk_multiedit.make_editable);	
 	},
-
 	form_to_json : function (value, key, indexed) {
 			var json = "";
 			$(key).each(function (index) {
@@ -69,6 +61,4 @@ var mk_multiedit = {
 			});
 			return $.parseJSON(json);
 	},
-
-
 }
--- readme.txt
+++ readme.txt
@@ -1,3 +1,112 @@
 mk_multiselect is a great inline editing tool. Which allows you to make many text into inputs by a simple click of a button. 
 
 a tutorial comming soon follow me on {twitter}[https://twitter.com/muhammad_khan40 ] for updates
+
+sorry for this but here is a really dirty example of how to use this:
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Using mk_multi select</title>
+    <style>
+    	body
+		{
+			width: 960px;
+			margin: 0 auto;
+		}
+
+		.contact
+		{
+			width: 450px;
+			background: #d0d1d0;
+			padding: .7em 1.5em;
+			margin: 1.3em;
+			overflow: hidden;
+			border-radius: 20px;
+			box-shadow: 1px 1px 10px rgba(0,0,0,.45);
+		}
+
+		.contact li
+		{
+			list-style: none;	
+		}
+
+		.contact li b
+		{
+			margin-right: 10px;
+		}
+
+		.contact:hover
+		{
+			background: #008686;
+			color: #fff;
+		}
+
+		.contact:hover h3
+		{
+			background: #012222;
+		}
+
+		.contact:hover a
+		{
+			color: #000d32;	
+		}
+
+		.contact img
+		{
+			background: #fff;
+			box-shadow: inset 1px 1px 10px rgba(0,0,0,.75);
+		}
+
+		.contact h3
+		{
+			background: #e4e5e5;
+			padding: .7em;
+			width: 100%;
+			box-shadow: inset 1px 1px 10px rgba(0,0,0,.1);
+		}
+
+		.fl
+		{
+			float: left;
+		}
+
+		.fr
+		{
+			float: right;
+		}
+
+		a
+		{
+			color: #005aff;
+		}
+    </style>
+    <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]--> 
+    <script src="./js/jquery-1.8.1.min.js"></script>
+</head>
+<body>
+	
+	<div class="contact">
+		<h3 class="name">Foo Bar</h3>
+		<div class="fl">
+			<img src="./images/blank-user-icon.png" alt="">
+		</div>
+		<div class="fr">
+			<ul>
+				<li><b >Address:</b><i class="edit">123 Main Street.</i></li>
+				<li><b>Phone:</b><i class="edit">1(111)222-3333</i></li>
+				<li><b>Email:</b><i class="edit">foo@example.org</i></li>
+				<li><b>Company:</b><i class="edit">example</i></li>
+				<li><b>Website:</b><a href="#" class="edit">example.org</a></li>
+			</ul>
+		</div>
+	</div>
+	<script src="../[html5]simpleToDo/mk_multiedit.js"></script>
+	<script>
+		mk_multiedit.init({ mainID: '.contact', parent: '.edit' });
+	</script>
+</body>
+</html>
