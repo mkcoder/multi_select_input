17278a55010225b4f51b0f501bb0f9020f616db5
Muhammad K. Khan <mk_muhammad.khan@hotmail.com>
2013-02-18 19:31:19 -0600

    Got cancel button working, and got a call back function
    |No Test Ran!| Got the cancel button to work, made a hidden input with
    the current text, when the cancel button is clicked it makes the _.text
    the hidden current value.

--- mk_multiedit.js
+++ mk_multiedit.js
@@ -4,16 +4,20 @@ var mk_multiedit = {
 		parent : 	 '#todoItem', // what elements are edit
 		json_keys: 	 '.edit', // where is the json keys attribute data-json on?,
 		json_values: '.edit', // where is the json value attribute data-json on?
-		closest: 	 'li' // is there an li on this page?
+		closest: 	 'li', // is there an li on this page?
+		callBack: 	 undefined, 
 	},
 	json : '',
 	init : function (config) { // MAIN function to which sets off the chain of events
 		$.extend(mk_multiedit.config, config)
 		$(mk_multiedit.config.parent).on('click', mk_multiedit.make_editable);	
-		mk_multiedit.submit();
+		mk_multiedit.submit(); 
 		mk_multiedit.cancel();
 		// first of all go get the data from the form and make store it in json
 		mk_multiedit.makeJson(mk_multiedit.config.json_keys);
+
+		// everything completes call the callback function
+		mk_multiedit.config.callBack === undefined || mk_multiedit.config.callBack()
 	},
 	makeJson: function ( /* element {class/data/id/item} that holds the key*/ key ) {
 		var key = new Array(), // e.g = data-json
@@ -26,10 +30,20 @@ var mk_multiedit = {
 		return mk_multiedit.form_to_json(key, value, false);
 	},
 	make_editable : function () {
+		// grab the parent
+		// then 
 		$(mk_multiedit.config.parent).each(function () {
 			var this_text = $(this).text();
-			$(this).text("");
 			var data_json = $(this).parent().find('input').data('json')
+			// this takes up space
+			// this is a work around for that
+			// please submit a better work around? NO CSS, JUST JS
+			$(this).text("");
+
+			// this is the value that will be when it cancels
+			$('<input />', { type: 'hidden', class: 'hidden', value: this_text, 'data-json': data_json}).appendTo(this);
+			
+			// this is the value that will submit
 			$('<input />', { type: 'text', class: 'edit', value: this_text, 'data-json': data_json}).appendTo(this);
 		});
 		$('<input/>', 
@@ -55,6 +69,10 @@ var mk_multiedit = {
 	}, 
 	cancel : function () { // TODO: cancel will return the form its orginal self;
 		$(mk_multiedit.config.mainID).on('click', '.cancel', function () {
+			
+			$(mk_multiedit.config.parent).each(function () {
+				$(this).text( $(this).find('.hidden').val() );// go get the element and set the item to the 
+			});
 			mk_multiedit.complete();
 		});
 	},
